a972d819aa78a93fc547f4674f3aa2018cd46df1
kcorkan <kcorkan@rallydev.com>
2015-12-08 22:01:16 -0600

    remove ability to edit planned velocity

--- src/javascript/timebox-column-progress-bar-override.js
+++ src/javascript/timebox-column-progress-bar-override.js
@@ -7,7 +7,7 @@
      */
     Ext.override(Rally.ui.gridboard.planning.TimeboxColumnProgressBar, {
         pointTotal: undefined,
-
+        allowEdit: false,
         _getTotalPointCount: function() {
             if (this.pointTotal){
                 return this.pointTotal;
@@ -17,6 +17,38 @@
                 var points = this._getRecordValue(card.getRecord(), this.pointField);
                 return Ext.isNumber(points) ? memo + points : memo;
             }, 0, this);
+        },
+        _showEditIcon: function() {
+           if (this.allowEdit){
+               this.getEl().down('.icon-edit').show();
+           }
+        },
+        _createTooltip: function(){
+            if (!this.allowEdit){
+                return;
+            }
+
+            if (this.tooltip) {
+                this._destroyTooltip();
+            }
+
+            this.tooltip = Ext.create('Rally.ui.tooltip.ToolTip', {
+                cls: 'set-planned-velocity-tooltip',
+                bodyStyle: 'text-align: center;',
+                width: 150,
+                anchor: 'top',
+                target: this._getProgressBarContainer(),
+                html: 'Edit Planned Velocity'
+            });
+        },
+        _onPlannedVelocityLinkClick: function(event) {
+            event.preventDefault();
+
+          if (this.allowEdit){
+              this._disableTooltip();
+              this._createPopoverWithData();
+          }
+
         }
     });
 })();
